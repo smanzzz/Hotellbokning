266bbb64c2fd5c788b8a7335bd2a52f7b3492957
Smanz <smanzhow@gmail.com>
2026-02-26 07:34:55 +0100

    build

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,62 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('hotell-app');
+export class AppComponent {
+  
+  inCheck = '';
+  utCheck = '';
+  roomType = '';
+  quantity: number | null = null;
+
+  
+  isBooked = false;
+  //referens till idag.
+  today: string = new Date().toISOString().split('T')[0];
+  maxDate: string = '2030-12-31';
+
+  showBooking() {
+    // Validering
+
+     const amount = Number(this.quantity);
+
+  if (!Number.isInteger(amount) || amount < 1 || amount > 10) {
+    alert("Antal personer måste vara ett heltal mellan 1 och 10.");
+    return;
+  }
+
+    if (!this.inCheck || !this.utCheck || !this.roomType || !amount) {
+      alert("Fyll i alla fält först!");
+      return;
+    }
+    
+    if (this.inCheck < this.today) {
+      alert("Incheckning kan inte vara i dåtid!");
+      return;
+    }
+
+    
+    if (this.utCheck <= this.inCheck) {
+      alert("Utcheckning måste vara minst en dag efter incheckning.");
+      return;
+    }
+
+    
+    const bokning = {
+      datum: `${this.inCheck} till ${this.utCheck}`,
+      rum: this.roomType,
+      personer: this.quantity
+    };
+    localStorage.setItem('hotellBokning', JSON.stringify(bokning));
+
+    
+    this.isBooked = true;
+  }
 }
